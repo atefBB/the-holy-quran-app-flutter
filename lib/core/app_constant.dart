eb3d83daf3b91b7fe80fe6185e6641e6ac4b0c7d
Atef Ben Ali <atef.bettaib@gmail.com>
2024-07-12 14:39:58 +0100

    fix: fix `app constants`

--- lib/core/app_constant.dart
+++ lib/core/app_constant.dart
@@ -3,7 +3,7 @@ class AppConstant {
 
   static const juz = 'الجزء';
   static const ajzaa = 'الأجزاء';
-  static const move = 'إنتقال';
+  static const move = 'انتقال';
   static const cancel = 'إلغاء';
   static const hizb = 'الحزب';
   static const hizbQuarter = 'ربع';
@@ -14,12 +14,12 @@ class AppConstant {
   static const index = 'الفهرس';
   static const makkiyah = 'مكية';
   static const madaniyah = 'مدنية';
-  static const searchAyah = 'إبحث عن آية';
+  static const searchAyah = 'ابحث عن آية';
   static const saveBookmark = 'حفظ علامة';
-  static const goToBookMark = 'الإنتقال إلى العلامة';
-  static const goToPage = 'إنتقال إلى الصفحة';
+  static const goToBookMark = 'الانتقال إلى العلامة';
+  static const goToPage = 'انتقال إلى الصفحة';
   static const saved = 'تم الحفظ';
   static const goBack = 'العودة';
 
-  static const noSearchYet = 'لم يتم إدراج هده الميزة بعد :(';
+  static const noSearchYet = 'لم يتم إدراج هده الميزة بعد';
 }
